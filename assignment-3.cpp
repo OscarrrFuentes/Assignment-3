ad3e9cc821ceb42008b256dea8215843fcecabc8
Oscar Fuentes <oscar.fuentes1412@gmail.com>
2024-03-03 20:47:25 +0000

    Sorted out input checks for particle setter functions and created detector classes

--- assignment-3.cpp
+++ assignment-3.cpp
@@ -10,7 +10,6 @@
 
 using std::string;
 
-const double light_speed{2.99792458e8};
 // Beginning of particle class
 class particle
 {
@@ -20,82 +19,176 @@ private:
   int charge;
   double velocity;
   double beta = velocity/light_speed;
-  //...other data members (see slides on BB)
 
 public:
   // Constructors
-  // Here you need a default constructor, and a parameterised constructor
   particle() = default;
   particle(string name, double mass, int construct_charge, double construct_velocity):
   particle_name{name}, rest_mass{mass}, charge{construct_charge}, velocity{construct_velocity}, beta{construct_velocity/light_speed}
   {}
+  particle(string name, double mass, int construct_charge, double construct_velocity, bool anti):
+  particle_name{name}, rest_mass{mass}, charge{construct_charge}, velocity{construct_velocity}, beta{construct_velocity/light_speed}
+  {if(anti){charge=-construct_charge; particle_name="anti-"+name;}}
   // Destructor
   ~particle(){std::cout<<"Destroying "<<particle_name<<std::endl;}
 
-  // Getter functions (accessors) to
+  // Getter functions
   string get_name(){return particle_name;}
   double get_mass(){return rest_mass;}
   int get_charge(){return charge;}
   double get_velocity(){return velocity;}
   double get_beta(){return beta;}
-  // This should include function returning beta value 
 
-  // Setter functions, to change value of data members
-  void set_name(std::string new_name) {if(check_string(new_name)){particle_name = new_name;}}
-  void set_mass(std::string new_mass) {if(check_double(new_mass)){rest_mass = stod(new_mass);}}
-  void set_charge(std::string new_charge) {if(check_int(new_charge)){charge=stoi(new_charge);}}
+  // Setter functions
+  void set_name(std::string new_name){particle_name = check_name(new_name);}
+  void set_mass(std::string new_mass){rest_mass = check_mass(new_mass);}
+  void set_charge(std::string new_charge){charge=check_charge(new_charge);}
   void set_velocity(std::string new_velocity)
   {
-    if (check_double(new_velocity))
-    {
-      velocity = stod(new_velocity);
-      beta = stod(new_velocity)/light_speed;
-    }
+    velocity = check_velocity(new_velocity);
+    beta = velocity/light_speed;
   }
-  // Make sure you check input validity before changing something
-  // Hint: you can use the input checking functions you used in assignment 1
 
-  // Function to print info about a particle
-  void print_data();
+  void print_data()
+  {
+    std::cout.precision(3);
+    std::cout<<"Particle: "<<particle_name<<"\nRest mass: "<<rest_mass<<" MeV\nCharge: "<<charge<<
+    " C\nVelocity: "<<velocity<<" m/s\nBeta = "<<beta<<"\n"<<std::endl;
+  }
 };
-
-// Implementation of print_data function goes here
-void particle::print_data()
-{
-  std::cout.precision(3);
-  std::cout<<"Particle: "<<particle_name<<"\nRest mass: "<<rest_mass<<" MeV\nCharge: "<<charge<<
-  " C\nVelocity: "<<velocity<<" m/s\nBeta = "<<beta<<"\n"<<std::endl;
-}
 // End of particle class and associated member functions
 
 // Beginning of detector class
+class detector
+{
+private:
+  std::string detector_type;
+  int particle_count;
+  int anti_particle_count;
 
-// Functionalities needed, in addition to constructor/destructor/setters/getters (see slides on BB):
-// - write a function that takes a particle 
-//   and returns a bool and a print-out on whether this type of detector detected the particle
-//   depending on the detector/particle combination (see slides on BB)
-// - have data members that keep track of how many particles were detected by this particular detector, 
-//   and of which type
-// - write a function (accessor) that prints how many particles passed through this detector
+public:
+  // Constructors:
+  detector() = default;
+  detector(std::string type):
+  detector_type{type}, particle_count{0}, anti_particle_count{0}{};
 
+  // Destructor
+  ~detector(){std::cout<<"Destroying "<<detector_type<<std::endl;}
+
+  // Getters
+  string get_type(){return detector_type;}
+
+  void show_particles()
+  {
+    if(detector_type == "tracker"){std::cout<<particle_count<<" electrons and "<<anti_particle_count<<" anti-electrons have passed through this tracker"<<std::endl;}
+    else if(detector_type == "muon chamber"){std::cout<<particle_count<<" muons and "<<anti_particle_count<<" anti-muons have passed through this muon chamber"<<std::endl;}
+    else if(detector_type == "calorimeter"){std::cout<<particle_count<<" tau particles and "<<anti_particle_count<<" anti-tau particles have passed through this calorimeter"<<std::endl;}
+    else{std::cout<<"DETECTOR_TYPE ERROR"<<std::endl;}
+  }
+  bool track_particle(particle lepton)
+  {
+    if(detector_type == "tracker")
+    {
+      if(lepton.get_name() == "electron")
+      {
+        particle_count++;
+        std::cout<<"Electron detected"<<std::endl;
+        return true;
+      }
+      else if(lepton.get_name() == "anti-electron")
+      {
+        anti_particle_count++;
+        std::cout<<"Anti-electron detected"<<std::endl;
+        return true;
+      }
+      return false;
+    }
+    else if(detector_type == "muon chamber")
+    {
+      if(lepton.get_name() == "muon")
+      {
+        particle_count++;
+        std::cout<<"Muon detected"<<std::endl;
+        return true;
+      }
+      else if(lepton.get_name() == "anti-muon")
+      {
+        anti_particle_count++;
+        std::cout<<"Anti-muon detected"<<std::endl;
+        return true;
+      }
+      return false;
+    }
+    else if(detector_type == "calorimeter")
+    {
+      if(lepton.get_name() == "tau")
+      {
+        particle_count++;
+        std::cout<<"Tau detected"<<std::endl;
+        return true;
+      }
+      else if(lepton.get_name() == "anti-tau")
+      {
+        anti_particle_count++;
+        std::cout<<"Anti-tau detected"<<std::endl;
+        return true;
+      }
+      return false;
+    }
+    std::cout<<"DETECTER ERROR"<<std::endl;
+    return false;
+  }
+};
 // End of detector class
 
 // Main program
 int main()
 {
-  particle electron("electron", 0.511, -1, 2.55e8);
-  electron.print_data();
-  std::string new_mass;
-  std::cout<<"Input new mass";
-  std::cin>>new_mass;
-  electron.set_mass(new_mass);
-  electron.print_data();
+  std::vector<particle> particles;
+  particles.emplace_back("electron", 0.511, -1, 2.55e8);
+  particles.emplace_back("electron", 0.511, -1, 1.01e8);
+  particles.emplace_back("muon", 105.7, -1, 500);
+  particles.emplace_back("muon", 105.7, -1, 2.98e8);
+  particles.emplace_back("muon", 105.7, -1, 3.06e6);
+  particles.emplace_back("muon", 105.7, -1, 100);
+  particles.emplace_back("tau", 1777, -1, 2.5e8);
+  particles.emplace_back("tau", 1777, -1, 2.2e8);
+  particles.emplace_back("tau", 1777, -1, 1.5e8);
+  particles.emplace_back("electron", 0.511, -1, 2.55e8, true);
+  particles.emplace_back("muon", 105.7, -1, 2.55e8, true);
+  particles.emplace_back("tau", 1777, -1, 2.55e8, true);
+
+  for(int i=0;i<particles.size();i++)
+  {
+    particles[i].print_data();
+  }
+
   // Create the following particles: 
   // two electrons, four muons, three taus, one antielectron, one antimuon, one antitau 
   // Use the parameterised constructor
 
   // Print out the data from all the particles (put them in a vector)
+  detector tracker{"tracker"};
+  detector muon_chamber{"muon chamber"};
+  detector calorimeter{"calorimeter"};
 
+  tracker.show_particles();
+  muon_chamber.show_particles();
+  calorimeter.show_particles();
+
+  std::cout<<"\nPassing particles through detectors...\n"<<std::endl;
+
+  for(auto& single_particle:particles)
+  {
+    tracker.track_particle(single_particle);
+    muon_chamber.track_particle(single_particle);
+    calorimeter.track_particle(single_particle);
+  }
+  std::cout<<"\n";
+  tracker.show_particles();
+  muon_chamber.show_particles();
+  calorimeter.show_particles();
+  std::cout<<"\n";
   // Create the following detectors: a tracker, a calorimeter, a muon chamber
 
   // Pass the list of particles into each detector
--- check_input.h
+++ check_input.h
@@ -2,14 +2,20 @@
 #ifndef check_input
 #define check_input
 
-
 #include<iostream>
 #include<string>
 #include<vector>
 #include<cmath>
+#include <cctype>
 
+const double light_speed{2.99792458e8};
 bool check_string(std::string input)
 {
+  if(input.size() == 0)
+  {
+    std::cout<<"That was not a valid input, please try again: ";
+    return false;
+  }
   for(const auto& chara:input)
   {
     if(!std::isalpha(chara))
@@ -24,12 +30,17 @@ bool check_string(std::string input)
 
 bool check_double(std::string input)
 {
+  if(input.size() == 0)
+  {
+    std::cout<<"That was not a valid input, please try again: ";
+    return false;
+  }
   for(const auto& chara:input)
   {
-    if(!std::isdigit(chara) && chara != '.')
+    if(!std::isdigit(chara) && chara != '.' && chara != '-')
     {
       std::cin.clear();
-      std::cout<<"That was not a valid input, please try again"<<std::endl;
+      std::cout<<"That was not a valid input, please try again:"<<std::endl;
       return false;
     }
   }
@@ -38,16 +49,91 @@ bool check_double(std::string input)
 
 bool check_int(std::string input)
 {
+  if(input.size() == 0)
+  {
+    std::cout<<"That was not a valid input, please try again: ";
+    return false;
+  }
   for(const auto& chara:input)
   {
-    if(!std::isdigit(chara))
+    if(!std::isdigit(chara) && chara != '-')
     {
-      std::cin.clear();
-      std::cout<<"That was not a valid input, please try again"<<std::endl;
+      std::cout<<"That was not a valid input, please try again: ";
       return false;
     }
   }
   return true;
+}
+
+double check_mass(std::string mass)
+{
+  if(check_double(mass))
+  {
+    if(std::stod(mass) <= 0)
+    {
+      std::cin.clear();
+      std::cout<<"The mass has to be greater than 0, please try again: ";
+      std::getline(std::cin, mass);
+      return check_mass(mass);
+    }
+    return std::stod(mass);
+  }
+  std::cin.clear();
+  std::getline(std::cin, mass);
+  return check_mass(mass);
+}
+
+double check_velocity(std::string velocity_input)
+{
+  if(check_double(velocity_input))
+  {
+    if(std::stod(velocity_input)<=-light_speed || std::stod(velocity_input)>light_speed)
+    {
+      std::cin.clear();
+      std::cout<<"The magnitude of the velocity can't be greater than the speed of light, please input again: ";
+      std::getline(std::cin, velocity_input);
+      return check_velocity(velocity_input);
+    }
+    return std::stod(velocity_input);
+  }
+  std::cin.clear();
+  std::getline(std::cin, velocity_input);
+  return check_velocity(velocity_input);
+}
+
+int check_charge(std::string charge)
+{
+  if(check_int(charge))
+  {
+    if(std::stoi(charge)!=-1 && std::stoi(charge)!=1)
+    {
+      std::cin.clear();
+      std::cout<<"The charge of a particle can only be +1 or -1, please input again: ";
+      std::getline(std::cin, charge);
+      return check_charge(charge);
+    }
+    return std::stoi(charge);
+  }
+  std::cin.clear();
+  if(charge[0] == '+'){ std::cout<<"Please be aware that '+1' is also not a valid input, only input 1"<<std::endl;}
+  std::getline(std::cin, charge);
+  return check_charge(charge);
+}
+
+std::string check_name(std::string name)
+{
+  for(int i{}; i<(name).length();i++)
+  {
+    name[i] = tolower(name[i]);
+  }
+  if(name!="electron" && name!="muon" && name!="tau" )
+  {
+    std::cin.clear();
+    std::cout<<"That lepton does not exist, please try again: ";
+    std::getline(std::cin, name);
+    return check_name(name);
+  }
+  return name;
 }
 
 #endif  //check_input
